ed7076ce58bd60c5edd08163f4d4fa2152990d60
Marcelo Castilho <>
2023-03-23 02:02:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+wallet = 41
+print ("Meu dinheiro na carteira é: ", wallet)
+debt = 20
+newWallet = wallet - debt
+print ("Meu dinheiro após o saque de ",debt, "reais é de: ",newWallet)
+
+day = 22
+month = 3
+print ("Hoje é dia ",day,"/",month,".")
