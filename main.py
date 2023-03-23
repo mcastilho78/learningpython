5ea496ff4f9b2423941e4891f5cd76ec6c0a5660
Marcelo Castilho <128659788+mcastilho78@users.noreply.github.com>
2023-03-23 19:32:02 +0000

    Atualização de código com strings e booleanos.

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ print ("Meu dinheiro após o saque de ",debt, "reais é de: ",newWallet)
 day = 22
 month = 3
 print ("Hoje é dia ",day,"/",month,".")
+
+loja = 'Marcelo\'s Camisetas "da hora"'
+print (loja)
+
+ator = "Brad Pitt"
+filme = "Guerra Mundial Z"
+print (f"O meu filme favorito é {filme} e o ator principal é {ator}")
+
+luz_acessa = False
+if luz_acessa:
+  print("A luz está acesa.")
+else:
+  print("A luz está apagada.")
+
+#comentário
